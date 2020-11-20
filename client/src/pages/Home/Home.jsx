437a41fe8fa96c2dfa12742a333b4566b6c35cc7
itchoib <itchoib@gmail.com>
2020-11-20 00:50:04 -0500

    removed home test

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
 const Home = () => {
-  console.log();
-  const handleClick = (e) => {
-    console.log({ [e.target.name]: e.target.innerText });
-  };
-
   return (
     <div>
       <h1> I am a home page </h1>
-      <div onClick={handleClick} value="hello">
-        {' '}
-        CLICK ME TEST{' '}
-      </div>
     </div>
   );
 };
