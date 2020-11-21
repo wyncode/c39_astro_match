4744831564902d27bf4483d42ef869f7ff8ea0ff
itchoib <itchoib@gmail.com>
2020-11-20 22:20:00 -0500

    initial login set up

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
-
+import './Login.css';
 const Login = () => {
   return (
-    <div>
-      <h1>I am a login page </h1>
+    <div className={'container'}>
+      <h1 className={'title-log'}> Astrodate </h1>
+      <p className={'tag-line'}>Find Love in the Stars! </p>
+      <p className={'help-text'}>
+        {' '}
+        By signing up for Astrodate, you agree to our Terms of Service . Learn
+        how we process your data in our Privacy Policy and Cookies Policy.{' '}
+      </p>
+      <button className={'sign-in-button'} type="submit">
+        {' '}
+        <p>Sign in with Apple</p>
+      </button>
+      <button className={'sign-in-button'} type="submit">
+        {' '}
+        <p>Sign in with Facebook</p>
+      </button>
+      <button className={'sign-in-button'} type="submit">
+        {' '}
+        <p>Sign in with Phone Number</p>
+      </button>
     </div>
   );
 };
