e576a61b64ed2265e588c99ce60340c65f2b4382
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-11-24 01:31:55 -0500

    merging head-01

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,10 @@ import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
-<<<<<<< HEAD
-import Profile from './pages/Profile/Profile';
-=======
 import Profile from './pages/Profile';
 import Preferences from './pages/Preferences/Preferences';
->>>>>>> e1b3b734fe5225745ea934042cc30f72bceb9db3
 import { AppContextProvider } from './context/AppContext';
 import './App.css';
-
 import Inbox from './pages/Inbox/Inbox';
 import Matches from './pages/Matches/Matches';
 import NavigationBar from './components/NavigationBar';
@@ -28,12 +23,8 @@ const App = () => {
           <Route exact path='/inbox' component={Inbox} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
-<<<<<<< HEAD
-          <Route exact path="/profile" component={Profile} />
-=======
           <PrivateRoute exact path="/profile" component={Profile} />
           <PrivateRoute exact path="/preferences" component={Preferences} />
->>>>>>> e1b3b734fe5225745ea934042cc30f72bceb9db3
         </Switch>
       </BrowserRouter>
     </AppContextProvider>
