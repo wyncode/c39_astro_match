373cc99ea3ed9a4913b83844ae18b9cc0b103d94
itchoib <69821336+itchoib@users.noreply.github.com>
2020-11-24 09:35:17 -0500

    Update App.jsx
    removed comment symbols

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,7 +15,7 @@ const App = () => {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route exact path="/login" component={Login} />
+          <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
           <PrivateRoute exact path="/profile" component={Profile} />
           <PrivateRoute exact path="/preferences" component={Preferences} />
