7a9a79777b58cfaa0afc7801ac23f64543a5a548
itchoib <itchoib@gmail.com>
2020-11-26 18:28:31 -0500

    added updates from r branch

--- client/src/pages/Inbox/Inbox.jsx
+++ client/src/pages/Inbox/Inbox.jsx
@@ -1,55 +1,63 @@
-// import React, { useState, useEffect } from 'react';
-// import axios from 'axios';
-// import './Inbox.css';
-// import { AppContext } from '../../context/AppContext';
+import React, { useState, useEffect, useContext } from 'react';
+import axios from 'axios';
+import { AppContext } from '../../context/AppContext';
+import './Inbox.css';
 
-// const Inbox = () => {
-//   useEffect(() => {
-//     // const [userInbox, setUserInbox] = useState([]);
-//     const { setCurrentUser } = useContext(AppContext);
+const Inbox = () => {
+  const [userInbox, setUserInbox] = useState([]);
+  const [conversation, setConversation] = useState();
+  const [user, setUser] = useState();
+  const [match, setMatch] = useState();
+  const { currentUser } = useContext(AppContext);
 
-//     axios
-//       .get(`/api/chat/${currentUser.inbox}`)
-//       .then((response) => {
-//         setConversation(response);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }, []);
+  useEffect(() => {
+    axios
+      .get(`/api/chat/${currentUser.inbox}`)
+      .then((response) => {
+        setConversation(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-//   if (conversation.participants[0] === currentUser.id) {
-//     let user = conversation.participants[0];
-//     let match = conversation.participants[1];
-//   } else {
-//     let user = conversation.participants[1];
-//     let match = conversation.participants[0];
-//   }
+  if (conversation.participants[0] === currentUser.id) {
+    setUser(conversation.participants[0]);
+    setMatch(conversation.participants[1]);
+  } else {
+    setUser(conversation.participants[1]);
+    setMatch(conversation.participants[0]);
+  }
 
-//   // Conversation, participants, message, time, id,
-//   return (
-//     <div>
-//       {userInbox.map((conversation.participants) => {
-//         <div className="background">
-//           <div className="title">YOUR MESSAGES</div>
+  // Conversation, participants, message, time, id,
+  return (
+    <div>
+      {userInbox.map((conversation) => {
+        return (
+          <>
+            <div className="background">
+              <div className="title">YOUR MESSAGES</div>
 
-//           <div className="incoming">
-//             <span className="avatar">{match.avatar}</span>
+              <div className="incoming">
+                <span className="avatar">{match.avatar}</span>
 
-//             <div className="messageBox">
-//               <div classsName="messageTitle">SUBJECT: HI!</div>
-//               <div className="text"> {conversation.participant.text} </div>
-//             </div>
-//           </div>
+                <div className="messageBox">
+                  <div classsName="messageTitle">SUBJECT: HI!</div>
+                  <div className="text"> {conversation.participant.text} </div>
+                </div>
+              </div>
 
-//           <div className="button">
-//             <span className="buttonText">Upgrade Membership</span>
-//           </div>
-//         </div>;
-//       })}
-//       ;
-//     </div>
-//   );
-// };
+              <div className="button">
+                <span className="buttonText">Upgrade Membership</span>
+              </div>
+            </div>
+            ;
+          </>
+        );
+      })}
+      ;
+    </div>
+  );
+};
 
-// export default Inbox;
+export default Inbox;
--- client/src/pages/Matches/Matches.jsx
+++ client/src/pages/Matches/Matches.jsx
@@ -1,54 +1,69 @@
-// import React from 'react';
-// import axios from 'axios';
-// import { AppContext } from '../../context/AppContext';
-// import './Matches.css';
-// //import pictures?//
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './Matches.css';
+//import pictures?//
 
-// const Matches = () => {
-//   const { setCurrentUser } = useContext(AppContext);
+const Matches = () => {
+  const [match, setMatch] = useState();
+  useEffect(() => {
+    axios
+      .get(`/api/users/matches/`)
+      .then((response) => {
+        setMatch(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-//     useEffect(() => {
-//     axios
-//       .get(`/api/users/match/${id}`);
-//       .then((response) => {
-//         setCurrentUser(response.data);
-//       })
-//       .catch((error) => {
-//         console.log(error);
-//       });
-//   }, [id]);
+  return (
+    <div className="background">
+      <h1 className="title">YOUR BEST MATCHES</h1>
+      {match.map((match) => {
+        return (
+          <>
+            <div className="matchProfile" id="linkToMatchProfile">
+              <div className="dynamicPicture">
+                <img src="" alt={match.name} className="matchImage" />
+                <span className="viewProfile">View Profile</span>
+                <img
+                  src={match.ascendant}
+                  alt={match.ascendant}
+                  className="ascendant"
+                />
+                <img src={match.moon} alt={match.moon} className="moon" />
+                <img
+                  src={match.starsign}
+                  alt={match.starsign}
+                  className="starsign"
+                />
+              </div>
+              <div className="matchStats">
+                <div className="name">{match.firstName}</div>
+                <div className="age">AGE: {match.age}</div>
+                <div className="location">
+                  {match.city}, {match.state}
+                </div>
+                <div className="sunMoonAscendant">
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </div>
+                <div className="percentageMatch">% Match</div>
+                <div className="matchBio">
+                  Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit
+                  amet, consectetur.Lorem ipsum dolor sit amet, consectetur.{' '}
+                </div>
+              </div>
+              <div className="button">
+                <span className="buttonText">Upgrade Membership</span>
+              </div>
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
+};
 
-//   return (
-//     <div className="background">
-
-//       <h1 className="title">YOUR BEST MATCHES</h1>
-//       {currentUser.matches.map((match) => {
-//              return(
-//         <div className="matchProfile" id="linkToMatchProfile">
-
-//             <div className="dynamicPicture">
-//              <img src="" alt={match.name} className="matchImage" />
-
-//                 <span className="viewProfile">View Profile</span>
-//                 <img src={match.ascendant} alt={match.ascendant} className="ascendant" />
-//                 <img src={match.moon} alt={match.moon} className="moon" />
-//                 <img src={match.starsign} alt={match.starsign} className="starsign" />
-//             </div>
-
-//             <div className="matchStats">
-//               <div className="name">{match.firstName}</div>
-//               <div className="age">AGE: {match.age}</div>
-//               <div className="location">{match.city}, {match.state}</div>
-//               <div className="sunMoonAscendant"><span></span><span></span><span></span></div>
-//               <div className="percentageMatch">{match.partnerPreference.zodiac.compatibility.score}% Match</div>
-//               <div className="matchBio">Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit amet, consectetur.Lorem ipsum dolor sit amet, consectetur. </div>
-//             </div>)   })}
-//             <div className="button" ><span className="buttonText">Upgrade Membership</span></div>
-
-//         </div>
-
-//     </div>
-//   )
-// };
-
-// export default Matches;
+export default Matches;
