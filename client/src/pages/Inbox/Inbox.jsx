99662bb7b2cc5a4d21a7db3375f0d4f320d3f02e
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-11-26 07:58:51 -0500

    api-calls for message to mongod establised

--- client/src/pages/Inbox/Inbox.jsx
+++ client/src/pages/Inbox/Inbox.jsx
@@ -4,54 +4,52 @@ import './Inbox.css';
 import { AppContext } from '../../context/AppContext';
 
 const Inbox = () => {
-useEffect(() => {
-  // const [userInbox, setUserInbox] = useState([]);
-  const { setCurrentUser } = useContext(AppContext);
-  
-      axios .get(`/api/chat/${currentUser.inbox}`)
-        .then((response) => {(
-          setConversation(response)
-        )})
-        .catch((error) => {
-          console.log(error);
-        });
-    }, []);
+  useEffect(() => {
+    // const [userInbox, setUserInbox] = useState([]);
+    const { setCurrentUser } = useContext(AppContext);
 
-if (conversation.participants[0] === currentUser.id) {
-  let user = conversation.participants[0];
-  let match = conversation.participants[1];
-} else {
-  let user = conversation.participants[1];
-  let match = conversation.participants[0];
-};
+    axios
+      .get(`/api/chat/${currentUser.inbox}`)
+      .then((response) => {
+        setConversation(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
+  if (conversation.participants[0] === currentUser.id) {
+    let user = conversation.participants[0];
+    let match = conversation.participants[1];
+  } else {
+    let user = conversation.participants[1];
+    let match = conversation.participants[0];
+  }
 
-Conversation, participants, message, time, id,
-
-const Inbox = () => {
+  // Conversation, participants, message, time, id,
   return (
     <div>
       {userInbox.map((conversation.participants) => {
-      <div className="background">
-        <div className="title">YOUR MESSAGES</div>
-    
-        <div className="incoming">
-          <span className="avatar">{match.avatar}</span>
+        <div className="background">
+          <div className="title">YOUR MESSAGES</div>
 
-          <div className="messageBox">
-            <div classsName="messageTitle">SUBJECT: HI!</div>
-            <div className="text"> {conversation.participant.text} </div>
+          <div className="incoming">
+            <span className="avatar">{match.avatar}</span>
+
+            <div className="messageBox">
+              <div classsName="messageTitle">SUBJECT: HI!</div>
+              <div className="text"> {conversation.participant.text} </div>
+            </div>
           </div>
-        </div>
 
-        <div className="button">
-          <span className="buttonText">Upgrade Membership</span>
-        </div>
-      </div>})};
+          <div className="button">
+            <span className="buttonText">Upgrade Membership</span>
+          </div>
+        </div>;
+      })}
+      ;
     </div>
   );
 };
-};
-
 
 export default Inbox;
--- client/src/pages/Inbox/InboxLayout/Inbox.jsx
+++ client/src/pages/Inbox/InboxLayout/Inbox.jsx
@@ -4,33 +4,36 @@ import axios from 'axios';
 import MessageCard from './MessageCard';
 import './Inbox.css';
 
-
 const Inbox = () => {
-
-  const App = () => {
-    const { setCurrentUser } = useContext(AppContext);
-      useEffect(() => {
-        axios
-          .get(`/api/users/${id}`)
-          .then((response) => {
-            setUser(response.data);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }, [id]);
+  const { setCurrentUser } = useContext(AppContext);
+  useEffect(() => {
+    axios
+      .get(`/api/users/${id}`)
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [id]);
 
   return (
     <div>
-      <div className="messageCard" >
-          {data.map((data, index) => {
-              return(
-                <MessageCard key="{data._id}" description="{data.lastMessage}" sender="{data.firstName}" imgName="{data.imgName}" />
-                );
-          })};
+      <div className="messageCard">
+        {data.map((data, index) => {
+          return (
+            <MessageCard
+              key="{data._id}"
+              description="{data.lastMessage}"
+              sender="{data.firstName}"
+              imgName="{data.imgName}"
+            />
+          );
+        })}
+        ;
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Inbox;
