ec764524ff3b3c76d0273761c8e3609fb523bea7
itchoib <69821336+itchoib@users.noreply.github.com>
2020-11-27 13:58:38 -0500

    Update App.jsx
    removed comments/nav bar from app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,24 +4,22 @@ import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import SignUp from './pages/SignUp/SignUp';
-// import Matches from './pages/Matches/Matches';
+import Matches from './pages/Matches/Matches';
 import Profile from './pages/Profile/Profile';
 import Preferences from './pages/Preferences/Preferences';
 
 import { AppContextProvider } from './context/AppContext';
 import './App.css';
-// import Inbox from './pages/Inbox/Inbox';
-import NavigationBar from './components/NavigationBar';
+import Inbox from './pages/Inbox/Inbox';
 
 const App = () => {
   return (
     <AppContextProvider>
       <BrowserRouter>
-      <NavigationBar />
         <Switch>
           <Route exact path="/" component={Home} />
-          {/* <Route exact path="/matches" component={Matches} /> */}
-          {/* <Route exact path='/inbox' component={Inbox} /> */}
+          <Route exact path="/matches" component={Matches} /> 
+          <Route exact path='/inbox' component={Inbox} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={SignUp} />
           {/* <Route exact path="/profile" component={Profile} /> */}
