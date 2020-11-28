023e51eecb758530ce88c5db247c958e6adf2c12
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-11-28 18:17:55 -0500

    matches page css with zodiac images

--- client/src/pages/Matches/Matches.jsx
+++ client/src/pages/Matches/Matches.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Matches.css';
+import Aquarius from '../../components/Images/Vectors/Matches/ZodiacSymbols/Signs/Glow/Aquarius.webp';
+import Leo from '../../components/Images/Vectors/Matches/ZodiacSymbols/Signs/Glow/Leo.jpg';
 //import pictures?//
 
 const Matches = () => {
@@ -10,35 +12,36 @@ const Matches = () => {
       .get(`/api/users/matches/`, { withCredentials: true })
       .then((response) => {
         setMatch(response.data);
+        console.log(response.data);
+        console.log(match);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [match]);
 
   return (
     <div className="background">
-      <h1 className="title">YOUR BEST MATCHES</h1>
-
-      {match &&
-        match.map((match) => {
-          return (
-            <>
+      <h1 className="intro">YOUR BEST MATCHES</h1>
+      <div className="matchCard">
+        {match &&
+          match.map((match) => {
+            return (
               <div className="matchProfile" id="linkToMatchProfile">
                 <div className="dynamicPicture">
                   <img src="" alt={match.name} className="matchImage" />
                   <span className="viewProfile">View Profile</span>
                   <img
-                    src={match.ascendant}
+                    src={Aquarius}
                     alt={match.ascendant}
                     className="ascendant"
                   />
-                  <img src={match.moon} alt={match.moon} className="moon" />
                   <img
-                    src={match.starsign}
-                    alt={match.starsign}
-                    className="starsign"
+                    src="https://static.displate.com/280x392/displate/2019-07-10/72e319e39e308bb5d06cd81bef1e8c3a_220ce183c372ebef78410fe2e74bfdb2.jpg"
+                    alt={match.moon}
+                    className="moon"
                   />
+                  <img src={Leo} alt={match.starsign} className="starsign" />
                 </div>
                 <div className="matchStats">
                   <div className="name">{match.firstName}</div>
@@ -54,57 +57,15 @@ const Matches = () => {
                   <div className="percentageMatch">% Match</div>
                   <div className="matchBio">{match.bio}</div>
                 </div>
-                <div className="button">
-                  <span className="buttonText">Upgrade Membership</span>
-                </div>
               </div>
-            </>
-          );
-        })}
+            );
+          })}
+      </div>
 
-      {match.map((match) => {
-        return (
-          <>
-            <div className="matchProfile" id="linkToMatchProfile">
-              <div className="dynamicPicture">
-                <img src="" alt={match.name} className="matchImage" />
-                <span className="viewProfile">View Profile</span>
-                <img
-                  src={match.ascendant}
-                  alt={match.ascendant}
-                  className="ascendant"
-                />
-                <img src={match.moon} alt={match.moon} className="moon" />
-                <img
-                  src={match.starsign}
-                  alt={match.starsign}
-                  className="starsign"
-                />
-              </div>
-              <div className="matchStats">
-                <div className="name">{match.firstName}</div>
-                <div className="age">AGE: {match.age}</div>
-                <div className="location">
-                  {match.city}, {match.state}
-                </div>
-                <div className="sunMoonAscendant">
-                  <span></span>
-                  <span></span>
-                  <span></span>
-                </div>
-                <div className="percentageMatch">% Match</div>
-                <div className="matchBio">
-                  Lorem ipsum dolor sit amet, consectetur. Lorem ipsum dolor sit
-                  amet, consectetur.Lorem ipsum dolor sit amet, consectetur.{' '}
-                </div>
-              </div>
-              <div className="button">
-                <span className="buttonText">Upgrade Membership</span>
-              </div>
-            </div>
-          </>
-        );
-      })}
+      <div className="button">
+        <span className="buttonText">Upgrade Membership</span>
+      </div>
+      <div className="footer"></div>
     </div>
   );
 };
