4905061df34ff6cce23833f55ff1e5413d066210
Rita Mwaikinda <srmwaikinda@gmail.com>
2020-11-29 15:08:01 -0500

    removed video-feature

--- server/app.js
+++ server/app.js
@@ -13,37 +13,6 @@ const express = require('express'),
 
 const app = express();
 
-//chat testing >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const { v4: uuidV4 } = require('uuid');
-
-app.set('view engine', 'ejs');
-// app.use(express(static('public')));
-
-app.get('/videochat', (req, res) => {
-  res.redirect(`/${uuidV4()}`);
-});
-
-app.get('/:room', (req, res) => {
-  res.render('room', { roomId: req.params.room });
-});
-
-io.on('connection', (socket) => {
-  socket.on('join-room', (roomId, userId) => {
-    // console.log(roomId, userId);
-    socket.join(roomId);
-    socket.to(roomId).broadcast.emit('user-connected', userId);
-
-    socket.on('disconnect', () => {
-      socket.to(roomId).broadcast.emit('user-disconnected', userId);
-    });
-  });
-});
-
-server.listen(3000);
-
-//chat testing <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 //Middleware
 app.use(express.json());
 
